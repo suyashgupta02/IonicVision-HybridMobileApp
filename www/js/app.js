2d797d16c78cbecd3215490f11a9920477b47df4
Suyash <suyash.gupta02@gmail.com>
2017-12-19 16:21:46 +0530

    First commit

---
+++ www/js/app.js
@@ -0,0 +1,25 @@
+angular.module('starter', ['ionic', 'ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('home', {
+    url: '/home',
+    templateUrl: 'templates/home.html'
+  });
+
+  $urlRouterProvider.otherwise('/home');
+});
